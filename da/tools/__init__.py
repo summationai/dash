555ff4785c889f68619f2638f129b19103f202ab
Ashpreet <ashpreetbedi@users.noreply.github.com>
2026-01-30 19:22:22 +0000

    init: data agent

--- da/tools/__init__.py
+++ da/tools/__init__.py
@@ -1,12 +1,10 @@
 """Data Agent Tools."""
 
-from da.tools.analyze import analyze_results
 from da.tools.introspect import create_introspect_schema_tool
 from da.tools.learnings import create_learnings_tools
 from da.tools.save_query import create_save_validated_query_tool
 
 __all__ = [
-    "analyze_results",
     "create_introspect_schema_tool",
     "create_learnings_tools",
     "create_save_validated_query_tool",
